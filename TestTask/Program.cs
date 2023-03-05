316b651ff37b6afd8002b2b71d201dcb8d139fa0
Roma <romabelka43@gmail.com>
2023-03-05 03:49:07 +0300

    initial commit

---
+++ TestTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories.DependencyInject;
+using Microsoft.Extensions.DependencyInjection;
+using TestTask.Services.DependencyInject;
+using TestTask.Validators.DependencyInject;
+
+namespace TestTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
